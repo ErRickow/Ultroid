530af783c3fb36e8ffc01b2bb682288d76bdf996
Just R <remixingmagelang@gmail.com>
2024-08-08 03:19:25 +0700

    update pyUltroid/_misc/_assistant.py

--- pyUltroid/_misc/_assistant.py
+++ pyUltroid/_misc/_assistant.py
@@ -79,7 +79,7 @@ def callback(data=None, from_users=[], admins=False, owner=False, **kwargs):
             if from_users and event.sender_id not in from_users:
                 return await event.answer("Bukan Untuk Lo!", alert=True)
             if owner and event.sender_id not in owner_and_sudos():
-                return await event.answer(f"This is {OWNER}'s bot!!")
+                return await event.answer(f"Ini adalah {OWNER}'s bot!!")
             try:
                 await func(event)
             except Exception as er:
