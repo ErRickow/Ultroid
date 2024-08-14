4cd4a82e471ce62b7c3dd86a541e832b8ac87247
Just R <remixingmagelang@gmail.com>
2024-08-15 02:53:29 +0700

    update pyUltroid/fns/helper.py

--- pyUltroid/fns/helper.py
+++ pyUltroid/fns/helper.py
@@ -137,7 +137,7 @@ if run_as_module:
             return await eod(
                 event, f"Please use `{HNDLR}install` as reply to a .py file."
             )
-        ok = await event.reply(event, "`Installing...`")
+        ok = await event.edit(event, "`Installing...`")
         reply = await event.get_reply_message()
         if not (
             reply.media
